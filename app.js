e41967309e9712225ce0339c2774b44a900bc250
CodingAesthete <harry_olli@abv.bg>
2024-01-12 14:37:13 +0200

    Add Auth

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const products = require('./routes/products');
 const people = require('./routes/people');
 const apiProducts = require('./routes/apiProducts');
+const auth = require('./routes/auth')
 
 app.use(express.static('./methods_public'))
 app.use(express.static('./navbar-app'))
@@ -11,15 +12,7 @@ app.use(express.json())
 app.use('/api/people', people)
 app.use('/api/products', products)
 app.use('/api', apiProducts)
-
-app.post('/login', (req, res) => {
-  const { name } = req.body
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`)
-  }
-
-  res.status(401).send('Please Provide Credentials')
-})
+app.use('/login', auth)
 
 app.listen(5000, () => {
   console.log('server is listening on port 5000...');
